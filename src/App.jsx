4fc6803d181a2b19d88b20ef233a12b3edcdae76
nair cardozo <nair_belenc@hotmail.com>
2023-03-02 10:21:03 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import  NavBar from "./components/NavBar/index";
+function App () {
+
+  return (
+  <div> 
+    {
+    }
+
+     < NavBar />
+  </div>
+  );
+}
+
+
+
+export default App
+
+
+
+
+        
+
+     
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,27 @@
+import "./navbar.css";
+
+function NavBar () {
+
+    return (
+        <div className="inicio">
+            <ul>
+                <li>
+                    <button>Farmacia</button>
+                </li>
+                <li>
+                    <button>Cósmetica</button>
+                </li>
+                <li>
+                    <button>Cuidados y belleza</button>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+
+
+
+
+
+export default NavBar;
